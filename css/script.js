0baab96bd53a3c950af35571c264cc3d7a3c180b
Emmanuel Adeyefa <adescoemmanel@gmail.com>
2025-07-12 17:22:48 +0100

    first commit

---
+++ css/script.js
@@ -0,0 +1,36 @@
+// //FORM VALIDATION
+
+// const form = document.getElementById("sign_up_form");
+// const f_name = form.elements["first_name"].value;
+// const l_name = form.elements["last_name"].value;
+// const email = form.elements["email"].value;
+// const password = form.elements["password"].value;
+// const confirmPassword = form.elements["confirm_password"].value;
+// const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// form.addEventListener("submit", function (event) {
+// function validateForm() {
+//   return re.test(String(email).toLowerCase());
+
+//   const validateEmail = (email) => {};
+
+//   if (
+//     f_name === "" ||
+//     l_name === "" ||
+//     email === "" ||
+//     password === "" ||
+//     confirmPassword === ""
+//   ) {
+//     alert("All fields are required.");
+//     return false;
+//   }
+
+//   if (!validateEmail(email)) {
+//     alert("Please enter a valid email address.");
+//     return false;
+//   }
+
+//   return true;
+// }
+
+// //FORM COLLECTION AND STORAGE
